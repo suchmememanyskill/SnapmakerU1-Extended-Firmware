72aae0c7a41bd0995dcc4f3c64fab67f50c5719a
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-02-25 23:15:50 +0100

    Add makefile to filament-detect repo

--- overlays/firmware-extended/15-external-filament-detection/scripts/01-clone-filament-detect.sh
+++ overlays/firmware-extended/15-external-filament-detection/scripts/01-clone-filament-detect.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GIT_URL=https://github.com/suchmememanyskill/filament-detect.git
-GIT_SHA=090c144e1f335e1f36236bbe53e812e9214e2d0a
+GIT_SHA=1d29557da53b0459a595444dc8fc94e444ffa461
 
 if [[ -z "$CREATE_FIRMWARE" ]]; then
   echo "Error: This script should be run within the create_firmware.sh environment."
@@ -14,6 +14,6 @@ TARGET_DIR="$CACHE_DIR/filament-detect"
 cache_git.sh "$TARGET_DIR" "$GIT_URL" "$GIT_SHA"
 
 echo ">> Installing filament-detect..."
-install -d "$ROOTFS_DIR/usr/local/bin/filament-detect"
-cp -r "$TARGET_DIR/." "$ROOTFS_DIR/usr/local/bin/filament-detect/"
+cd "$TARGET_DIR" 
+make install-u1 DESTDIR="$ROOTFS_DIR/usr/local/bin/filament-detect/"
 echo ">> filament-detect installation completed successfully."
